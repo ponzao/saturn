e4cad2a66d75433a14a4b97c587caf38c649e2ee
Vesa Marttila <vesa.marttila@helsinki.fi>
2010-01-13 09:53:27 +0200

    Added factorial function. BUG: It overflows because I am using "int".

--- saturn.c
+++ saturn.c
@@ -16,8 +16,24 @@ static int average(lua_State *L) {
     return 1;
 }
 
+static int fac(int n) {
+    return n <= 1 ? 1 : n * fac(n - 1);
+}
+
+static int factorial(lua_State *L) {
+    int n = lua_tonumber(L, -1);
+    printf("%d\n", n);
+    
+    int result = fac(n);
+    
+    lua_pushnumber(L, result);
+
+    return 1;
+}
+
 static const luaL_reg saturn[] = {
     {"average", average},
+    {"factorial", factorial},
     {NULL, NULL}
 };
 
